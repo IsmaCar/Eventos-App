5dc728e2dbbfa7e117f6a36d525c994d3981d0ff
IsmaCar <160272744+IsmaCar@users.noreply.github.com>
2025-04-14 15:41:58 +0200

    Update Backend

--- Backend/config/bundles.php
+++ Backend/config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];

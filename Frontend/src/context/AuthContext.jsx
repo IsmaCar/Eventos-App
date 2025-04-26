94da4141c2605fc173f654401dd9e63b76e351ec
IsmaCar <160272744+IsmaCar@users.noreply.github.com>
2025-04-26 15:30:27 +0200

    Create authentication context

---
+++ Frontend/src/context/AuthContext.jsx
@@ -0,0 +1,72 @@
+import { createContext, useContext, useState } from "react";
+
+const API_URL = import.meta.env.VITE_API_URL;
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+
+  const registerUser = async ({ username, email, password })=> {
+    try {
+        const response = await fetch(`${API_URL}/api/register`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ username, email, password })
+        })
+
+        if(!response.ok)
+            throw new Error("Error al registrarse");
+        
+         await response.json();
+        
+        
+    } catch (error) {
+        console.log('Error al registrarse');
+    }
+}
+
+  const loginUser = async ({ email, password }) => {
+    try {
+      const response = await fetch(`${API_URL}/api/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Usuario o contraseña incorrectos");
+      }
+      // Si estoy aquí es porque el usuario se ha logueado correctamente
+      const data = await response.json();
+      setUser(data.user);
+      setToken(data.token);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user", JSON.stringify(data.user));
+    } catch (error) {
+      console.log("Error al iniciar sesión", error);
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  };
+  return (
+    <AuthContext.Provider value={{ user, token, registerUser,loginUser, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth debe estar dentro del proveedor AuthProvider");
+  }
+  return context;
+};

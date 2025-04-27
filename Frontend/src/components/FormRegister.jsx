e93dd540ef55c84fd54ad8305a140dcb6235a37d
IsmaCar <160272744+IsmaCar@users.noreply.github.com>
2025-04-27 11:30:18 +0200

    Update FormRegister add checks and error messages

--- Frontend/src/components/FormRegister.jsx
+++ Frontend/src/components/FormRegister.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { useState } from "react";
 
-function RegisterForm () {
+function RegisterForm () { 
     const [formData, setFormData] = useState({
             username: '',
             email: '',
@@ -10,26 +10,54 @@ function RegisterForm () {
         })
     const { registerUser } = useAuth();
     const navigate = useNavigate();
+    const [error, setError] = useState('')
 
+        //Función para manejar el cambio de los inputs
         const handleChange = (e) => {
             const nombre = e.target.name;
 
             setFormData({...formData, [nombre]: e.target.value.trim()})
         }
 
+        //Función para manejar el submit del formulario
+        // Se encarga de validar los datos y llamar a la función registerUser
         const handleSubmit = async (e) => {
             e.preventDefault()
+            setError('')
             try {
-                await registerUser(formData)
-                navigate('/login')
+                if(!formData.username || !formData.email || !formData.password) {
+                    setError('Todos los campos son obligatorios')
+                    return
+                }
+
+                const result = await registerUser(formData)
+
+                if(result && result.success) {
+                    navigate('/login')
+                } else {
+                    setError(result.message || 'Error al registrarse')
+                }
+               
             } catch (error) {
-                
+                console.log('Error al registrarse;', error)
+
+                if (error.response && error.response.data) {
+                    setError(error.response.data.error || 'Error del servidor');
+                } else {
+                    setError('Error al conectar con el servidor');
+                }
             }
         }
 
     return (
         <div className='max-w-md mx-auto my-10 p-5 bg-white rounded-lg shadow-lg'>
         <h2 className='text-2xl font-semibold text-center text-gray-800'>Registrarse</h2>
+
+        {error && (
+                <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-4 mb-4">
+                    {error}
+                </div>
+            )}
 
         <form onSubmit={handleSubmit} className='space-y-4'>
             <div>
@@ -42,11 +70,11 @@ function RegisterForm () {
                 required
                 placeholder="nombre de usuario"
                 className='w-full px-4 py-2 text-lg text-gray-900 border 
-                                border-gray-300 rounded-lgfocus:outline-none focus:border-gray-500'/>
+                                border-gray-300 rounded-lg focus:outline-none focus:border-gray-500'/>
             </div>
             <div>
                 <label htmlFor="email" className='block text-xl font-semibold text-gray-900'></label>
-                <input type="text" 
+                <input type="email" 
                 id="email"
                 name="email"
                 value={formData.email}
@@ -54,7 +82,7 @@ function RegisterForm () {
                 required
                 placeholder="correo"
                 className='w-full px-4 py-2 text-lg text-gray-900 border 
-                                border-gray-300 rounded-lgfocus:outline-none focus:border-gray-500'/>
+                                border-gray-300 rounded-lg focus:outline-none focus:border-gray-500'/>
             </div>
             <div>
                 <label htmlFor="password" className='block text-xl font-semibold text-gray-900'></label>
@@ -66,7 +94,7 @@ function RegisterForm () {
                 required
                 placeholder="contraseña"
                 className='w-full px-4 py-2 text-lg text-gray-900 border 
-                                border-gray-300 rounded-lgfocus:outline-none focus:border-gray-500'/>
+                                border-gray-300 rounded-lg focus:outline-none focus:border-gray-500'/>
             </div>
             <button type="submit" 
                     className='w-full px-4 py-2 text-lg font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-700'
